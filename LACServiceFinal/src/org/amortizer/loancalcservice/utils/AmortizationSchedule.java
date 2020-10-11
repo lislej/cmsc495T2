faf660790e036a18808da8a8193ebfebce3ce8dc
James Lisle <lislej@gmail.com>
2020-10-11 15:02:41 -0400

    service updates

--- LACServiceFinal/src/org/amortizer/loancalcservice/utils/AmortizationSchedule.java
+++ LACServiceFinal/src/org/amortizer/loancalcservice/utils/AmortizationSchedule.java
@@ -2,6 +2,8 @@ package org.amortizer.loancalcservice.utils;
 
 
 import java.io.IOException;
+import java.text.DecimalFormat;
+
 import org.amortizer.loancalcservice.email.EmailManager;
 import com.sendgrid.Response;
 
@@ -17,7 +19,11 @@ public class AmortizationSchedule {
 
 		double balance = loanAmt;
 		double payment = loanPmt;
-		double rate    = intRate;
+		DecimalFormat df3 = new DecimalFormat("0.###");
+		double rate = new Double(df3.format(intRate));
+		
+		
+		
 	    int    term    = lnTerm;
 	    double totalInt = 0.0;
 	    
@@ -28,7 +34,7 @@ public class AmortizationSchedule {
 		
 		StringBuffer amortScheduleBody = new StringBuffer();
 		
-		amortScheduleBody.append("<b><pre>Month   Payment   Principal    Interest       Balance</b></pre><br>");
+		amortScheduleBody.append("<b><pre>Month    Payment   Principal    Interest       Balance</b></pre><br>");
 
 		for (int i = 0; i < term; i++) {
 
@@ -37,8 +43,8 @@ public class AmortizationSchedule {
 			principal = calculatePrincipal(payment, monthlyInterest);
 			balance = calculateBalance(balance, 0, principal);
 
-			amortScheduleBody.append(String.format("<b><pre> %3d    %7.2f     %7.2f     %7.2f     %8.2f</b></pre>", i + 1, payment, principal, monthlyInterest, balance > 0 ? balance:0.00));
-			amortScheduleBody.append("\n");
+			amortScheduleBody.append(String.format("<b><pre> %4d    %7.2f     %7.2f     %7.2f     %9.2f</b></pre>", i + 1, payment, principal, monthlyInterest, balance > 0 ? balance:0.00));
+//			amortScheduleBody.append("\n");
 
 		}
 
@@ -48,13 +54,9 @@ public class AmortizationSchedule {
 		amortScheduleHdr.append("<b>-----------------Loan Amortization Schedule-----------------</b>");
 
 		amortScheduleHdr.append(String.format("<b><pre>Loan Amount ......: %10.2f</b></pre>", loanAmt));
-		amortScheduleHdr.append("\n");
 		amortScheduleHdr.append(String.format("<b><pre>Loan Payment .....: %10.2f</b></pre>", loanPmt));
-		amortScheduleHdr.append("\n");
-		amortScheduleHdr.append(String.format("<b><pre>Interest Rate ....: %10.2f</b></pre>", rate));
-		amortScheduleHdr.append("\n");
+		amortScheduleHdr.append(String.format("<b><pre>Interest Rate ....: %10.3f</b></pre>", rate));
 		amortScheduleHdr.append(String.format("<b><pre>Loan Term ........: %10d</b></pre>", lnTerm));
-		amortScheduleHdr.append("\n");
 		amortScheduleHdr.append(String.format("<b><pre>Interest Paid ....: %10.2f</b></pre>", totalInt));
 		
 		amortScheduleHdr.append("<br>");
@@ -62,13 +64,13 @@ public class AmortizationSchedule {
 		
 		//assemble final document
 		StringBuffer amortSchedule = new StringBuffer();
-		amortSchedule.append("<html>\r\n");
+		amortSchedule.append("<html>");
 		amortSchedule.append("<font face=courier new size=2px >");
 		amortSchedule.append("<body>");
 		amortSchedule.append(amortScheduleHdr.toString());
 		amortSchedule.append(amortScheduleBody.toString());
-		amortSchedule.append("</font>");
 		amortSchedule.append("</body>");
+		amortSchedule.append("</font>");		
 		amortSchedule.append("</html>");
 		
 		return amortSchedule.toString();
@@ -95,7 +97,7 @@ public class AmortizationSchedule {
 	public static void main (String[] args) throws IOException {
 		
 		AmortizationSchedule am = new AmortizationSchedule();
-		String schedule = am.createAmortizationSchedule(150000, 1000, 5, 180, 0);
+		String schedule = am.createAmortizationSchedule(223225.75, 1250, 5.3754, 360, 0);
 		
 		System.out.println(schedule);
 		
